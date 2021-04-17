7add6ac41707ceaa7fe0f9dfe799f25dcdc1c58e
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-04-17 08:58:54 +0530

    Update __main__.py

--- Mizuki/__main__.py
+++ Mizuki/__main__.py
@@ -619,7 +619,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I am now online!")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Liebe has been deployed successfully !")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"

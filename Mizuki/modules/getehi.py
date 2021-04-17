a5ba0652e381c4ecc62989431c23aed51550cf6d
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-04-17 23:09:44 +0530

    Update getehi.py

--- Mizuki/modules/getehi.py
+++ Mizuki/modules/getehi.py
@@ -4,7 +4,7 @@ from telegram.ext import CallbackContext, run_async
 from Mizuki import dispatcher
 from Mizuki.modules.disable import DisableAbleCommandHandler
 
-EHI_STRINGS = "Here, new ehi files by @TheMizukiBot 👸\n\n👉 https://da.gd/yn1NQ"
+EHI_STRINGS = "Here, new ehi files by @Liebe_Op_Bot 👸\n\n👉 https://da.gd/yn1NQ"
 
 
 @run_async

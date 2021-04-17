895117236d3844be37eb302653d6e197fe09d7d8
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-04-17 11:47:55 +0530

    Update fun.py

--- Mizuki/modules/fun.py
+++ Mizuki/modules/fun.py
@@ -268,45 +268,6 @@ weebyfont = [
     "丫",
     "乙",
 ]
-ABUSE_STRINGS = [
-    "`Owww ... Such a stupid idiot.`",
-    "`Don't drink and type.`",
-    "`Command not found. Just like your brain.`",
-    "`Bot rule 544 section 9 prevents me from replying to stupid humans like you.`",
-    "`Sorry, we do not sell brains.`",
-    "`Believe me you are not normal.`",
-    "`I bet your brain feels as good as new, seeing that you never use it.`",
-    "`If I wanted to kill myself I'd climb your ego and jump to your IQ.`",
-    "`You didn't evolve from apes, they evolved from you.`",
-    "`What language are you speaking? Cause it sounds like bullshit.`",
-    "`You are proof that evolution CAN go in reverse.`",
-    "`I would ask you how old you are but I know you can't count that high.`",
-    "`As an outsider, what do you think of the human race?`",
-    "`Ordinarily people live and learn. You just live.`",
-    "`Keep talking, someday you'll say something intelligent!.......(I doubt it though)`",
-    "`Everyone has the right to be stupid but you are abusing the privilege.`",
-    "`I'm sorry I hurt your feelings when I called you stupid. I thought you already knew that.`",
-    "`You should try tasting cyanide.`",
-    "`You should try sleeping forever.`",
-    "`Pick up a gun and shoot yourself.`",
-    "`Try bathing with Hydrochloric Acid instead of water.`",
-    "`Go Green! Stop inhaling Oxygen.`",
-    "`God was searching for you. You should leave to meet him.`",
-    "`You should Volunteer for target in an firing range.`",
-    "`Try playing catch and throw with RDX its fun.`",
-    "`People like you are the reason we have middle fingers.`",
-    "`When your mom dropped you off at the school, she got a ticket for littering.`",
-    "`You’re so ugly that when you cry, the tears roll down the back of your head…just to a`",
-    "`If you’re talking behind my back then you’re in a perfect position to kiss my a**!.`",
-    "`Hey Remdi......Tell your giving rates.`"
-]
-
-@run_async
-def ABUSE(update: Update, context: CallbackContext):
-    args = context.args
-    message = update.effective_message
-    string = ""
-
 
 @run_async
 def weebify(update: Update, context: CallbackContext):
@@ -367,7 +328,6 @@ DECIDE_HANDLER = DisableAbleCommandHandler("decide", decide)
 TABLE_HANDLER = DisableAbleCommandHandler("table", table)
 SHOUT_HANDLER = DisableAbleCommandHandler("shout", shout)
 WEEBIFY_HANDLER = DisableAbleCommandHandler("weebify", weebify)
-ABUSE_HANDLER = DisableAbleCommandHandler("abuse", abuse)
 
 dispatcher.add_handler(WEEBIFY_HANDLER)
 dispatcher.add_handler(SHOUT_HANDLER)
@@ -382,7 +342,6 @@ dispatcher.add_handler(BLUETEXT_HANDLER)
 dispatcher.add_handler(RLG_HANDLER)
 dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
-dispatcher.add_handler(ABUSE_HANDLER)
 
 __mod_name__ = "Fun 😜"
 __command_list__ = [
@@ -414,5 +373,4 @@ __handlers__ = [
     SANITIZE_HANDLER,
     SHOUT_HANDLER,
     WEEBIFY_HANDLER,
-    ABUSE_HANDLER,
 ]

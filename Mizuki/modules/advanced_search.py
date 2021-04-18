d05dff0f9579043f9d35b76e015951b7ad7c7ec2
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-04-18 13:48:12 +0530

    Update advanced_search.py

--- Mizuki/modules/advanced_search.py
+++ Mizuki/modules/advanced_search.py
@@ -3,7 +3,6 @@
 import logging
 
 from pyrogram.types import Message
-from search_engine_parser import GoogleSearch
 from youtube_search import YoutubeSearch
 
 from Mizuki import pbot as app
@@ -18,87 +17,6 @@ import pyrogram
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-# google
-
-
-@app.on_message(pyrogram.filters.command(["google"]))
-@capture_err
-async def google(_, message: Message):
-    try:
-        if len(message.command) < 2:
-            await message.reply_text('"/google" Needs An Argument')
-            return
-        text = message.text.split(None, 1)[1]
-        gresults = await GoogleSearch().async_search(text, 1)
-        result = ""
-        for i in range(4):
-            try:
-                title = gresults["titles"][i].replace("\n", " ")
-                source = gresults["links"][i]
-                description = gresults["descriptions"][i]
-                result += f"[{title}]({source})\n"
-                result += f"`{description}`\n\n"
-            except IndexError:
-                pass
-        await message.reply_text(result, disable_web_page_preview=True)
-    except Exception as e:
-        await message.reply_text(str(e))
-
-
-# StackOverflow [This is also a google search with some added args]
-
-
-@app.on_message(pyrogram.filters.command(["so"]))
-@capture_err
-async def stack(_, message: Message):
-    try:
-        if len(message.command) < 2:
-            await message.reply_text('"/so" Needs An Argument')
-            return
-        gett = message.text.split(None, 1)[1]
-        text = gett + ' "site:stackoverflow.com"'
-        gresults = await GoogleSearch().async_search(text, 1)
-        result = ""
-        for i in range(4):
-            try:
-                title = gresults["titles"][i].replace("\n", " ")
-                source = gresults["links"][i]
-                description = gresults["descriptions"][i]
-                result += f"[{title}]({source})\n"
-                result += f"`{description}`\n\n"
-            except IndexError:
-                pass
-        await message.reply_text(result, disable_web_page_preview=True)
-    except Exception as e:
-        await message.reply_text(str(e))
-
-
-# Github [This is also a google search with some added args]
-
-
-@app.on_message(pyrogram.filters.command(["gh"]))
-@capture_err
-async def github(_, message: Message):
-    try:
-        if len(message.command) < 2:
-            await message.reply_text('"/gh" Needs An Argument')
-            return
-        gett = message.text.split(None, 1)[1]
-        text = gett + ' "site:github.com"'
-        gresults = await GoogleSearch().async_search(text, 1)
-        result = ""
-        for i in range(4):
-            try:
-                title = gresults["titles"][i].replace("\n", " ")
-                source = gresults["links"][i]
-                description = gresults["descriptions"][i]
-                result += f"[{title}]({source})\n"
-                result += f"`{description}`\n\n"
-            except IndexError:
-                pass
-        await message.reply_text(result, disable_web_page_preview=True)
-    except Exception as e:
-        await message.reply_text(str(e))
 
 
 # YouTube

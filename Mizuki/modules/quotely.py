2a0122c030c5432f5c60aa42eb1363de07fbafcb
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-05-04 10:34:27 +0530

    Update quotely.py

--- Mizuki/modules/quotely.py
+++ Mizuki/modules/quotely.py
@@ -9,7 +9,7 @@ from fontTools.ttLib import TTFont
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import functions, types
 
-from DaisyX.services.events import register
+from Mizuki.events import register
 
 COLORS = [
     "#F07975",
